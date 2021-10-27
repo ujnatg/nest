f2ea25a3327d2ab25160f8ede9bccb6abade83ce
Eugen Degtyarev <ujnatg@gmail.com>
2021-10-27 03:54:58 +0300

    Update report.py

--- report.py
+++ report.py
@@ -13,7 +13,7 @@ build_id = sys.argv[1]
 
 template_map = (
     ['Paypal Express checkout', {iphone: ['US', 'GB']}, {android: ['US', 'GB']}, {pc_chrome: ['US', 'GB']}, {pc_edge: ['US', 'GB']}, {mac: ['US', 'GB']}],
-    ['ApplePay checkout', {iphone: ['US', 'GB']}, {android: ['US', 'GB']},  {pc_chrome: ['US', 'GB']}, {pc_edge: ['US', 'GB']}, {mac: ['US', 'GB']}],
+    ['ApplePay checkout', {iphone: ['US', 'GB']}, ,  , , {mac: ['US', 'GB']}],
     ['Guest checkout', {iphone: ['US', 'CA', 'GB', 'EU', 'DE']}, {android: ['US', 'CA', 'GB', 'EU', 'DE']}, {pc_chrome: ['US', 'CA', 'GB', 'EU', 'DE']}, {pc_edge: ['US', 'CA', 'GB', 'EU', 'DE']}, {mac: ['US', 'CA', 'GB', 'EU', 'DE']}],
     ['Paypal checkout', {iphone: ['US', 'CA', 'GB', 'EU', 'DE']}, {android: ['US', 'CA', 'GB', 'EU', 'DE']}, {pc_chrome: ['US', 'CA', 'GB', 'EU', 'DE']}, {pc_edge: ['US', 'CA', 'GB', 'EU', 'DE']}, {mac: ['US', 'CA', 'GB', 'EU', 'DE']}],
     ['Afterpay checkout', {iphone: ['US', 'GB']}, {android: ['US', 'GB']}, {pc_chrome: ['US', 'GB']}, {pc_edge: ['US', 'GB']}, {mac: ['US', 'GB']}],
@@ -35,6 +35,8 @@ def load_results():
 
 
 def s(test, platform_country):
+    if platform_country == None:
+        return ''
     result = ''
     for country in list(platform_country.values())[0]:
         country_result = ''
